36ab3d4a9aaa69624773a4d06af5ae2662d2e959
Natasza Nowakowska <natasza.nowakowska@postnord.com>
2024-12-13 09:55:26 +0100

    add purchase test

--- tests/store.purchase.spec.ts
+++ tests/store.purchase.spec.ts
@@ -1,11 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { StorePage } from '../pages/store-page';
 
-test.describe('Purchase Flow', () => {
-    let storePage: StorePage;
+test.describe('Store Purchase Flow', () => {
+    test('Make a purchase for a product', async ({ page }) => {
+        const storePage = new StorePage(page);
 
-    test.beforeEach(async ({ page }) => {
-        storePage = new StorePage(page);
         await storePage.goToPage();
+        expect(await storePage.getPageHeading()).toContain('Store');
+
+        await storePage.selectProduct('Cup of Coffee');
+
+        await storePage.enterQuantity(5);
+
+        await storePage.clickBuyButton();
+
+        const confirmationMessage = await page.locator('#message')
+        await expect(confirmationMessage).toContainText('You bought');
+        await expect(confirmationMessage).toContainText('Cup of Coffee');
     });
 });
