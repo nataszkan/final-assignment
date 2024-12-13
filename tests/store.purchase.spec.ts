861e663ca9caa8fe774b0cc89435520d13bc29e9
Natasza Nowakowska <natasza.nowakowska@postnord.com>
2024-12-13 10:03:10 +0100

    update with variables for products and quantity

--- tests/store.purchase.spec.ts
+++ tests/store.purchase.spec.ts
@@ -1,21 +1,22 @@
 import { test, expect } from '@playwright/test';
 import { StorePage } from '../pages/store-page';
 
+const PRODUCT_NAME = 'Cup of Coffee';
+const QUANTITY = 8;
+
 test.describe('Store Purchase Flow', () => {
     test('Make a purchase for a product', async ({ page }) => {
         const storePage = new StorePage(page);
-
+    
         await storePage.goToPage();
         expect(await storePage.getPageHeading()).toContain('Store');
-
-        await storePage.selectProduct('Cup of Coffee');
-
-        await storePage.enterQuantity(5);
-
+    
+        await storePage.selectProduct(PRODUCT_NAME);
+        await storePage.enterQuantity(QUANTITY);
         await storePage.clickBuyButton();
-
-        const confirmationMessage = await page.locator('#message')
+    
+        const confirmationMessage = page.locator('#message');
         await expect(confirmationMessage).toContainText('You bought');
-        await expect(confirmationMessage).toContainText('Cup of Coffee');
+        await expect(confirmationMessage).toContainText(PRODUCT_NAME);
     });
 });
